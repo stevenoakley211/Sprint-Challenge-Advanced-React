2aca6e3c58ac909d4b826ff9780cd13743078baf
steven oakley <stevenoakley211@live.com>
2020-01-24 21:09:17 -0600

    Finished Class component

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import WorldPlayer from './compponents/WorldPlayer'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  state = {
+    players:[]
+  };
+
+  componentDidMount() {
+    fetch('http://localhost:5000/api/players')
+      .then(res=> res.json())
+      .then(playersList =>{
+        console.log(playersList)
+         this.setState({players: playersList})
+      })
+  }
+  render(){
+    return(
+      <div>
+        {this.state.players.map(players =>{
+        return <WorldPlayer player={players.name} country={players.country} searches={players.searches} />
+        })}
+      </div>
+    )
+  }
 }
 
 export default App;
